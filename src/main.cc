abc76f5f4c0d4b4d85673e12aab0227175cb8c15
randypalusz <ibanezguy998@gmail.com>
2021-07-19 15:35:22 -0400

    added sfml as submodule

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,19 @@
 #include <iostream>
+#include <SFML/Window.hpp>
 
-int main() { std::cout << "hello" << std::endl; }
+int main() {
+  sf::Window window(sf::VideoMode(800, 600), "My window");
+
+  // run the program as long as the window is open
+  while (window.isOpen()) {
+    // check all the window's events that were triggered since the last
+    // iteration of the loop
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      // "close requested" event: we close the window
+      if (event.type == sf::Event::KeyPressed) window.close();
+      if (event.type == sf::Event::Closed) window.close();
+    }
+  }
+  std::cout << "hello" << std::endl;
+}
