32e1c69dc7b3ced0b2db165a99e1086ecb13b463
randypalusz <ibanezguy998@gmail.com>
2021-07-25 23:35:25 -0400

    fixed bar width int truncation

--- src/GraphDisplay.cpp
+++ src/GraphDisplay.cpp
@@ -30,10 +30,11 @@ bool GraphDisplay::update(const std::vector<int>& in) {
   }
   m_window.clear(sf::Color::Blue);
   int numBars = in.size();
-  int widthPerBar = m_size.x / numBars;
+  float widthPerBar = (float)m_size.x / (float)numBars;
   float min = *(std::min_element(in.begin(), in.end()));
   float max = *(std::max_element(in.begin(), in.end()));
   float topBottomBorder = 25.0f;
+  float widthBuffer = 1.0f;
   // ensure the height of the bar doesn't reach the top of the window
   float newMax = m_size.y - (2.0f * topBottomBorder);
   // ensure the lowest value in the list is still visible
@@ -44,8 +45,8 @@ bool GraphDisplay::update(const std::vector<int>& in) {
     float normalizedHeight =
         normalize((float)in.at(i), min, max, newMin, newMax);
     // TODO: modulate width of the bar based on how many elements there are
-    sf::RectangleShape bar =
-        sf::RectangleShape(sf::Vector2f(widthPerBar - 5.0f, normalizedHeight));
+    sf::RectangleShape bar = sf::RectangleShape(
+        sf::Vector2f(widthPerBar - widthBuffer, normalizedHeight));
     bar.setPosition(sf::Vector2f(
         currentBarX, (m_size.y - normalizedHeight) - topBottomBorder));
     m_window.draw(bar);
